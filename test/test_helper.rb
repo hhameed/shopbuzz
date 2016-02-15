7e03b7c06086e95aad16f6ce67254630dd0ae60b
rishabh92 <rishabh.zhcet@gmail.com>
2016-02-14 19:42:54 -0600

    test_environment_setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ require 'simplecov'
 SimpleCov.start 'rails'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-
+#test
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
