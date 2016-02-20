ac83430c146b50b2b8f06a461cfa3a29626d48c0
rishabh92 <rishabh.zhcet@gmail.com>
2016-02-19 23:44:00 -0600

    Product Comparison tried to consider all exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, :with => :rescue404
+  rescue_from ActionController::RoutingError, :with => :rescue404
+
+  def rescue404
+    raise "dsjsdk"
+  end
+
 end
