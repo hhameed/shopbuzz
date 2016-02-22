2916f0bae2b84d360acdf0e274b9a6ae2967cba2
Haider Hameed <haiderhameed@gmail.com>
2016-02-21 19:31:55 -0600

    browse by category page connected to the main website

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -5,7 +5,7 @@ class SiteController < ApplicationController
   end
 
   def browse
-    @products = Product.paginate(page: params[:page], per_page: 5)
+    @products = Product.where(params.permit(:category_id)).paginate(page: params[:page], per_page: 5)
   end
 
 end
