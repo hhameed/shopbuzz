de7f38a9b7bd4f30c40d686c27ac71bd40402672
rishabh92 <rishabh.zhcet@gmail.com>
2016-03-10 23:07:56 -0600

    dummy_products complete

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
 
 
-  get 'compare' => 'compares#compare'
+  #
 
   root 'site#index'
   get 'site/browse'
@@ -62,8 +62,11 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
+  get 'compare' => 'compares#compare'
   resources :products do
     resources :product_reviews
   end
+
+
+
 end
