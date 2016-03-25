01d7960aab1d069afde4840eb3497a4272aa05d6
Balaji Thiruppathi <Balaji@Balajis-MacBook-Pro.local>
2016-03-25 12:30:29 -0500

    currently issues with slider values being passed

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -35,3 +35,22 @@
     }
 
     $(RP.setup);
+
+
+    $(document).ready(function() {
+        var slider=$( "#slider-range" ).slider({
+            range: true,
+            min: 0,
+            max: 500,
+            values: [ 75, 250 ],
+            slide: function( event, ui ) {
+                $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+                console.log(slider.slider("values"));
+                $("#data1").val(slider.slider("values"));
+            }
+
+        });
+        $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+            " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+
+    });
