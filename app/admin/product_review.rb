871c1209459ff203897e6f3df2b1deed5436b589
Balaji <bala92@gmail.com>
2016-04-03 16:57:50 -0500

    minor test change

--- app/admin/product_review.rb
+++ app/admin/product_review.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register ProductReview do
-
+  config.sort_order ="id_asc"
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
