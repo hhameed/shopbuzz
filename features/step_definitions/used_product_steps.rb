abca0874449e299a887b3405111edd00561981c3
kiranbichugatti <kiran.bichugatti@gmail.com>
2016-04-16 18:52:17 -0500

    cucumber tests for sell used products

--- features/step_definitions/used_product_steps.rb
+++ features/step_definitions/used_product_steps.rb
@@ -1,24 +1,19 @@
-# Given(/^the following products have been added to the Used Product$/) do |table|
-#   table.hashes.each do |usedproduct|
-#     # Each returned user will be a hash representing one row of the user_table
-#     # The keys will be the table headers and the values will be the row contents.
-#     # Entries can be directly to the database with ActiveRecord methods
-#     # Add the necessary Active Record call(s) to populate the database.
-#     p = UsedProduct.new(usedproduct)
-#     p.save(:validate => false)
-#   end
-# end
-# Given(/^I am on the Homepage$/) do
-#   visit root_path;
-# end
-#
-# When(/^I click the usedproduct page link$/) do
-#   #page.all(:link,"1")
-#   visit "/used_products/page_by_category?category_id=1"
-# end
-#
-# Then(/^I should see the usedproduct page$/) do
-#   #visit used_products_page_by_category_path(Product.find_by_category_id(1));
-#   #visit "/used_products/page_by_category?category_id=1"
-#   #expect(page).to have_content("Used Digital Cameras")
-# end
+Given(/^I am on the Product page$/) do
+  visit product_path(Product.find(1));
+end
+
+When(/^I click the sell usedproduct page link$/) do
+  visit  new_product_used_product_path(Product.find(1))
+end
+
+When(/^I fill in the form and click submit$/) do
+  fill_in "Name", :with=>'kiran'
+  fill_in "Email", :with=>'kiran@gmail.com'
+  fill_in "Description", :with=>'Good'
+  fill_in "Price", :with=> 30000
+  find('#used_product_city_id').find("option[value='1']").click
+  script2 = "document.getElementById('warranty').value = 'No Warranty'"
+  script3 = "document.getElementById('usage_duration').value = '1-3 Month Used'"
+  fill_in "Tel Number", :with=>"35161618"
+  click_button "Submit Your Used iphone 5 16GB"
+end
