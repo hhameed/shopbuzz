44ad37836b07781fb24e51fa68cd347d1302e535
rishabh92 <rishabh.zhcet@gmail.com>
2016-04-17 18:56:43 -0500

    Integration test for admin

--- app/admin/used_product.rb
+++ app/admin/used_product.rb
@@ -2,7 +2,19 @@ ActiveAdmin.register UsedProduct do
   config.sort_order ="id_asc"
   index do
 
-    column :name;    column :price;    column :condition_ex,:sortable => false;    column :additional_info, :sortable => false;column :warranty, :sortable => false; column :usage_duration, :sortable => false ;column :contact_number, :sortable => false    ; column :email, :sortable => false ; column :created_at  ; column :updated_at ; column :city ; column :category, :sortable => false  ; column :product
+    column :name
+    column :price
+    column :condition_ex,:sortable => false
+    column :additional_info, :sortable => false
+    column :warranty, :sortable => false
+    column :usage_duration, :sortable => false
+    column :contact_number, :sortable => false
+    column :email, :sortable => false
+    column :created_at
+    column :updated_at
+    column :city
+    column :category, :sortable => false
+    column :product
 
   end
 end
