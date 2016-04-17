69a8353cb20900b7d22217be6af019d3829c60e6
Haider Hameed <haiderhameed@gmail.com>
2016-04-17 18:25:13 -0500

    integration testing complete for user flow new products

--- spec/integration/user_workflow_new_products_spec.rb
+++ spec/integration/user_workflow_new_products_spec.rb
@@ -6,19 +6,52 @@ RSpec.describe "workflow of the user interested in buying new products:", type: 
     load "#{Rails.root}/db/seeds.rb"
   end
   it "he should be able to navigate through all scenerios using the links in the website" do
+
     visit root_path
+
     expect(page).to have_content("Samsung Galaxy Note II N7100 16GB")
+
     expect(page).to_not have_content("Price Range")
+
     find('#cat_id_4').click
+
     expect(page).to have_content("Price Range")
+
     expect(page).to_not have_content("Sony Phone ABC")
+
     find('#brand_id_2').set(true)
+
     find('#form_submit').click
+
     expect(page).to have_content("Sony Phone ABC")
+
     find('#brand_id_2').set(false)
+
     find('#form_submit').click
+
     expect(page).to have_content("Samsung Galaxy Note II N7100 16GB")
-    find(:xpath, '//*[@id="ajaxproducts"]/div[1]/div/h4/a').click
+
+    expect(page).to_not have_content("Samsung Galaxy XYZ")
+
+    find('#search').set("galaxy x")
+
+    find('#search_submit', visible: false).click
+
+    expect(page).to_not have_content("Samsung Galaxy Note II N7100 16GB")
+
+    expect(page).to have_content("Samsung Galaxy XYZ")
+
+    find('#cat_id_4').click
+
+    find('#product_url_id_1').click
+
     expect(page).to have_content("New Samsung Galaxy Note II N7100 16GB Prices in Pakistan")
+
+    find('#spl_1').click
+
+    @spl = SellerProductLink.find(1)
+
+    expect(page.body).to include(@spl.url)
+
   end
 end
